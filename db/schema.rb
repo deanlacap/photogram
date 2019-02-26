7e1eccac8dd97118c6618c933304d55b8ffeda19
deanlacap <deanlacap@gmail.com>
2019-02-26 13:49:16 -0800

    Add location column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_26_205058) do
+ActiveRecord::Schema.define(version: 2019_02_26_214739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_02_26_205058) do
     t.string "phone_number", limit: 15
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["session_token"], name: "index_users_on_session_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
